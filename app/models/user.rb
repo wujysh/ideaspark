9c100ba538d0c49ea690a93bba55a70c2391b059
Erick Guan <fantasticfears@gmail.com>
2014-02-19 14:25:38 +0800

    add uniqueness check for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 64 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { minumum: 5, maximum: 320 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end

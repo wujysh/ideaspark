d3aa9a04660f8a06e980937593c6854b11253fb2
Erick Guan <fantasticfears@gmail.com>
2014-02-19 16:49:26 +0800

    update route for user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 IdeaSpark::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root  'static_pages#home'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new',          via: 'get'
+  match '/about',  to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

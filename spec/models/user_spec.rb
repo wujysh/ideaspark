c2d4187a15a7deea71ad60ae58b1a678759b49ab
Erick Guan <fantasticfears@gmail.com>
2014-02-19 15:33:21 +0800

    add virtual password field

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example", email: "user@example.com") }
+  before { @user = User.new(name: "Example", email: "user@example.com",
+                            password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 

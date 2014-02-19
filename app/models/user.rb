e0a366b591e2ec77a6a4fe68d3c921d90ffbde7a
Erick Guan <fantasticfears@gmail.com>
2014-02-19 15:15:35 +0800

    update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 64 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { minumum: 5, maximum: 320 },

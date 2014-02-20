807057e66535ede15c1fd342839d85156d5ded59
Erick Guan <fantasticfears@gmail.com>
2014-02-20 13:09:11 +0800

    update assets

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module IdeaSpark
   class Application < Rails::Application
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.eot *.woff *.ttf)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

7589f58de51fcb9b10fe90de321162e82ca92c8b
Erick Guan <fantasticfears@gmail.com>
2014-02-21 00:30:01 +0800

    update

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get authentication_pages_index_path
-      expect(response.status).to be(200)
-    end
+describe "Authentication page" do
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
   end
 end

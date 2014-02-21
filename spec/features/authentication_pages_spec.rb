a8867876ca7dbe269df4fdc5c3415b706eb82668
Erick Guan <fantasticfears@gmail.com>
2014-02-21 18:09:25 +0800

    auth test

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -32,4 +32,22 @@ describe "Authentication page" do
       it { should_not have_link('Sign in', :href => signin_path) }
     end
   end
+
+  describe "authorization" do
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+        describe "visiting the edit page" do
+          before { visit profile_edit_path }
+          it { should have_title('Sign in') }
+        end
+      end
+      
+      describe "submitting to the update action", type: :request do
+        before { patch user_path(user) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
+  end
 end

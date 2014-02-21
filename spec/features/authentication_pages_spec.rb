5ea05a90841b5294d06613ef53fd485fc90e232d
Erick Guan <fantasticfears@gmail.com>
2014-02-21 09:17:53 +0800

    update session

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -6,7 +6,23 @@ describe "Authentication page" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_content('Sign in') }
+      it { should have_title('Sign in') }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email", :with => user.email.upcase
+        fill_in "Password", :with => user.password
+        click_button "Sign in"
+      end
+      
+      it { should have_link ('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
   end
 end

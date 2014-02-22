7c77de84417484e4138f5b6bec4ae8a9ef087b09
Erick Guan <fantasticfears@gmail.com>
2014-02-22 21:28:45 +0000

    update user model for revise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_reader :admin
-  before_save { email.downcase! }
-  before_create :create_remember_token
-
-  validates :name, presence: true, length: { maximum: 64 }
- 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 
-  validates :email, presence: true, length: { minumum: 5, maximum: 320 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-
-  has_secure_password
-  validates :password, length: { minimum: 6 }
-
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-
-  private
-   
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
 end

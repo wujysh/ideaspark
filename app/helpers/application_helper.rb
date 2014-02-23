cc084898f206083e822c6bb72d79e60e299d9605
Erick Guan <fantasticfears@gmail.com>
2014-02-23 21:13:38 +0000

    adjust for bootstrap alert class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,9 @@ module ApplicationHelper
   def flash_class(level)
     case level
       when :notice        then 'alert alert-info'
+      when :warning       then 'alert alert-warning'
       when :success       then 'alert alert-success'
-      when :error, :alert then 'alert alert-error'
+      when :error, :alert then 'alert alert-danger'
     end
   end
 end

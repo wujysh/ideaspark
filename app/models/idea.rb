59f9626f848631eb4848e694e695315c3444b3b0
Erick Guan <fantasticfears@gmail.com>
2014-02-23 14:31:56 +0000

    shine model idea user

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ActiveRecord::Base
+  acts_as_votable
+  acts_as_commontable
+  acts_as_taggable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   rolify
+  acts_as_voter
+  acts_as_commontator
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
